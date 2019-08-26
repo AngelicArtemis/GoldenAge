30d70f026188377fd9c1b340caff77bda87811d7
Bunbun <s3587133@student.rmit.edu.au>
2019-08-27 02:42:08 +1000

    photo hint effect now work on collision with torii hint c:
    will implement it on the other colliders

---
+++ Assets/script/ParticleStuff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleStuff : MonoBehaviour
+{
+
+    private bool playStars = false;
+    public GameObject stars;
+    public UnityEngine.Vector3 position; //put in the position through inspector
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hewwo?am i in the circle?");
+            Explode();
+        }
+    }
+    void Explode()
+    {
+        GameObject starsEffect = Instantiate(stars, position, Quaternion.identity);
+        starsEffect.GetComponentInChildren<ParticleSystem>().Play();
+        //Destroy(starsEffect);
+    }
+
+}
