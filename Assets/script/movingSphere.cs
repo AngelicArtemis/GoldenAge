d64c2937718c3a932a2210c519653ed2bc07c5d3
Bunbun <51069350+angelicarty@users.noreply.github.com>
2019-09-24 18:17:25 +1000

    testing stuff

---
+++ Assets/script/movingSphere.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingSphere : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.TransformPoint(Vector3.zero);
+    }
+    void Update()
+    {
+        if(Input.GetKey("i"))
+        {
+            Debug.Log("i");
+            moveUp();
+        }
+        if (Input.GetKey("k"))
+        {
+            moveDown();
+        }
+        if (Input.GetKey("l"))
+        {
+            moveRight();
+        }
+        if (Input.GetKey("j"))
+        {
+            moveLeft();
+        }
+
+    }
+
+    void moveRight()
+    {
+        this.gameObject.transform.Translate(Vector3.right);
+
+    }
+
+    void moveLeft()
+    {
+        this.gameObject.transform.Translate(Vector3.left);
+
+    }
+
+    void moveUp()
+    {
+        this.gameObject.transform.Translate(Vector3.up);
+
+
+    }
+
+    void moveDown()
+    {
+        this.gameObject.transform.Translate(Vector3.down);
+
+    }
+}
---
+++ Assets/script/testingSignedAngle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testingSignedAngle : MonoBehaviour
+{
+    public GameObject sphere;
+    Vector3 spherePosition;
+    public GameObject cube; //the cube is the one mirror 
+    Vector3 cubePosition;
+    public GameObject sphere2;
+    Vector3 sphere2Position;
+
+    private void Start()
+    {
+        spherePosition = sphere.transform.position;
+        cubePosition = cube.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        spherePosition = sphere.transform.position;
+        cubePosition = cube.transform.position;
+
+
+
+        Vector3 targetDir = spherePosition - cubePosition;
+        Vector3 forward = cube.transform.forward;
+        float angle = Vector3.SignedAngle(targetDir, forward, new Vector3(0.0f,1,0.0f));
+
+        sphere2.transform.position = cubePosition - targetDir;
+        sphere2Position = sphere2.transform.position;
+        sphere2Position.x = targetDir.x;
+        sphere2.
+        sphere2.transform.position = sphere2Position;
+
+
+
+        if (Input.GetKey("b"))
+        Debug.Log(angle);
+        //Quaternion rotateangle = Quaternion.Euler(0f, 0f, 90f);
+
+        Debug.DrawLine(spherePosition, cubePosition, Color.blue);
+        Debug.DrawRay(cubePosition, targetDir, Color.magenta);
+
+        Debug.DrawRay(cubePosition, forward, Color.cyan);
+        Debug.DrawRay(spherePosition, forward,Color.red);     //this one fucking works
+    }
+}
