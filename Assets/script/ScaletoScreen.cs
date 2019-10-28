ec5576e0ea1ba9ed0d569d4d961c3bd1b6e986f6
Bunbun <51069350+angelicarty@users.noreply.github.com>
2019-10-28 17:55:54 +1100

    scale

---
+++ Assets/script/ScaletoScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaletoScreen : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        
+        var size = gameObject.transform as RectTransform;
+        size.sizeDelta = new Vector2(Screen.width, Screen.height);
+    }
+}
